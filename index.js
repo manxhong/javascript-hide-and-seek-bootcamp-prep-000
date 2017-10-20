b3db129a87a70ad339c0f6a6833c1456a049a4fe
Man Hong Lee <manhong323@gmail.com>
2017-10-20 21:59:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('.target');
+}
+
+function deepestChild(){
+  const lis = document.querySelectorAll('#grand-node div');
+  return lis[lis.length-1];
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML)+n).toString();
+  }
+}
